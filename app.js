2ecd2db1b79b8be3147d69cdb703907e9876152e
Corwin <corwin_hope@yahoo.com>
2024-02-05 16:06:16 -0500

    Update

--- app.js
+++ app.js
@@ -5280,7 +5280,7 @@ class MToolBar extends ToolBar {
       this.addToolButton("link", {
         text: "&Help",
         cb: function () {},
-        href: "https://easy-grapher.herokuapp.com/grapherHelp/Grapher.html",
+        href: "./grapherHelp/Grapher.html",
         target: "_blank",
         class: "noSelect",
         tooltip: "Launches online help.",
